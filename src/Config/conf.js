74b073f4de1e571dcf89e408533801e5fb49b5f7
saksham762007 <samrathore07062007@gmail.com>
2026-04-05 12:45:18 +0530

    feat: add configuration file for Appwrite environment variables

--- src/Config/conf.js
+++ src/Config/conf.js
@@ -1,9 +1,9 @@
 const conf = {
-    appwrite: String(import.meta.env.VITE_APPWRITE_URL),
-    appproject: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    appcollection: String(import.meta.env.VITE_APPWRITE_COLLECTIONS_ID),
-    appbucket: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-    appdatabase: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appWrite: String(import.meta.env.VITE_APPWRITE_URL),
+    appProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTIONS_ID),
+    appBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+    appDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
 }
 
 
