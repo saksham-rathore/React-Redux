e9f234b603be8118b2adb0ad4bcbda5d84212e63
saksham762007 <samrathore07062007@gmail.com>
2026-04-07 02:00:45 +0530

    feat: initialize Redux store, auth slice, and Appwrite authentication service with basic layout components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import AuthService from "./appwrite/auth"
+import {useDispatch} from 'react-redux'
+import {login, logout} from "./stores/authslice"
+import {Footer, Header} from "./Components"
 
 const App = () => {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setloading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <div>A blog with appwrite</div>
+  useEffect(() => {
+    AuthService.getCurrentUser
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    }
   )
+    .finally(() => setloading(false))
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Headers /> 
+        <main>
+          {/* //outlet */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
